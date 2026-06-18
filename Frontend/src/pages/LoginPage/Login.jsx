4fa2f607779eb785a153baf459cfc2ec06dd690b
AdrianoE-Dgz <areliedgz5005@gmail.com>
2026-06-18 17:17:36 -0600

    FEAT: Formulario Controlado Registro

--- Frontend/src/pages/LoginPage/Login.jsx
+++ Frontend/src/pages/LoginPage/Login.jsx
@@ -1,10 +1,24 @@
 import { useNavigate, useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
 import { logUser } from '/src/httpRequests';
 import './Login.css'
 
 function Login() {
+  const [logUsername, setUsername] = useState(null);
+  const [logPassword, setPassword] = useState(null);
+
   const navigate = useNavigate();
   const { authCallback } = useOutletContext();
+
+  const handleUsername = (e) => {
+    const text = e.target.value;
+    setUsername(text);
+  }
+
+  const handlePassword = (e) => {
+    const text = e.target.value;
+    setPassword(text);
+  }
 
   const loginUser = async (e) => {
     e.preventDefault();
@@ -36,11 +50,12 @@ function Login() {
       <form id="loginForm" onSubmit={loginUser}>
         <div className="mb-3">
           <label for="loginUsername" className="form-label">Usuario</label>
-          <input type="text" className="form-control" id="loginUsername" />
+          <input type="text" className="form-control" value={logUsername} onChange={handleUsername} />
+          
         </div>
         <div className="mb-3">
           <label for="loginPassword" className="form-label">Contraseña</label>
-          <input type="password" className="form-control" id="loginPassword" />
+          <input type="password" className="form-control" value={logPassword} onChange={handlePassword} />
         </div>
           <div className='text-center mb-3'>
           <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
