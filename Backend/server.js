510ff634c9317c07801a8be63d8f629421d81a2c
AdrianoE-Dgz <areliedgz5005@gmail.com>
2026-06-20 09:50:28 -0600

    FIX: CORSN'T

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 // server.js
 require('dotenv').config({ override: true });
 const express = require('express');
-const cors = require('cors');
 
 const productRoutes = require('./routes/productos.routes.js');
 const pedidosRoutes = require('./routes/pedidos.routes.js');
@@ -9,7 +8,6 @@ const authRoutes = require('./routes/auth.routes.js');
 const pool = require('./db/conexion.js'); // <-- Importamos la conexión
 const app = express();
 const PORT = 3000;
-app.use(cors());
 
 app.use(express.json());
 
